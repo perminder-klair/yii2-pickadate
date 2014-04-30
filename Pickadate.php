72ba8bb97c91dd48643a93385af55486e92cccc5
parminder <perminder.klair@gmail.com>
2014-04-30 17:27:59 +0100

    added bootstrap classes

--- Pickadate.php
+++ Pickadate.php
@@ -9,6 +9,31 @@ use yii\helpers\Json;
 
 class Pickadate extends \yii\widgets\InputWidget
 {
+    /**
+     * @var string the size of the input - 'lg', 'md', 'sm', 'xs'
+     */
+    public $size;
+
+    /**
+     * @var string/boolean the addon content
+     */
+    public $addon = '<i class="glyphicon glyphicon-time"></i>';
+
+    /**
+     * @var array HTML attributes for the addon container
+     * the following special options are identified
+     * - asButton: boolean if the addon is to be displayed as a button.
+     * - buttonOptions: array HTML attributes if the addon is to be
+     *   displayed like a button. If [[asButton]] is true, this will
+     *   default to ['class' => 'btn btn-default']
+     */
+    public $addonOptions = [];
+
+    /**
+     * @var array HTML attributes for the input group container
+     */
+    public $containerOptions = [];
+
     public $language = 'en';
     public $initJs = null;
     public $isTime = false;
@@ -37,7 +62,7 @@ class Pickadate extends \yii\widgets\InputWidget
         Yii::setAlias('@pickadate', dirname(__FILE__));
         $this->registerAsset();
 
-        echo $this->renderInput();
+        echo Html::tag('div', $this->renderInput(), $this->containerOptions);
     }
 
     /**
@@ -45,6 +70,11 @@ class Pickadate extends \yii\widgets\InputWidget
      */
     protected function renderInput()
     {
+        Html::addCssClass($this->options, 'form-control');
+        if (!empty($this->options['disabled'])) {
+            Html::addCssClass($this->addonOptions, 'disabled-addon');
+        }
+
         if ($this->hasModel()) {
             $input = Html::activeTextInput($this->model, $this->attribute, $this->options);
         } else {
